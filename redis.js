06b33fb64c829bc1e853e691434683afdc394276
Gabriel447 <fullstackgabe@gmaill.com>
2025-03-19 19:04:28 -0300

    prompt ajust

--- redis.js
+++ redis.js
@@ -6,7 +6,9 @@ const redis = new Redis({
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
   password: process.env.REDIS_PASSWORD,
-  tls: {},
+  tls: {
+    rejectUnauthorized: false
+  }
 });
 
 redis.on("error", (err) => {
